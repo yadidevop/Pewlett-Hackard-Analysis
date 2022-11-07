2dc0a96f4b3650627592f13ee6c3d4a30d507159
Yadidevop <yadidevop@gmail.com>
2022-11-07 17:32:27 -0500

    schema code updated to correct tables

--- schema.sql
+++ schema.sql
@@ -40,21 +40,14 @@ CREATE TABLE titles (
   	title VARCHAR NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
-  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES dept_emp (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-    hire_date DATE NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES titles (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
-
-SELECT * FROM departments;
